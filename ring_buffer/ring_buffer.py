0d6e58125afa479be300c8c0f03792df80d48664
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2020-03-20 21:08:47 -0400

    i am very behind

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,34 +8,22 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        # if storage is 0, add the item to the tail and set current to that item
-        if self.storage.length == 0:
-            self.storage.add_to_tail(item)
-            self.current = self.storage.tail
         #  if storage is less than capacity, add the item to the tail and set current to the new head
-        elif self.storage.length < self.capacity:
+        if self.storage.length < self.capacity:
             self.storage.add_to_tail(item)
             self.current = self.storage.head
         # If storage is higher than capacity, remove from the head (oldest value), add to tail
-        elif self.storage.length >= self.capacity:
-            self.storage.remove_from_head()
-            self.storage.add_to_tail(item)
-            # Just in case this is the first value and the capacity is very small make the added value the tail
-            if self.storage.head == self.current:
-                self.current = self.storage.tail
+        else:
+            self.current.value = item
+            self.current = self.current.next or self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-        if self.current is None:
-            return None
         iterable = self.storage.head
-        list_buffer_contents.append(iterable.value)
-        while iterable is not self.storage.tail:
+        while iterable is not None:
+            list_buffer_contents.append(iterable.value)
             iterable = iterable.next
-
-        # TODO: Your code here
-
         return list_buffer_contents
 
 
